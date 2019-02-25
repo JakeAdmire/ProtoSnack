af7f219f6113c487a71f64f705719bd1340fca6a
Jake <jakeadmire1@gmail.com>
2019-02-25 10:49:38 -0700

    Add seperate styling to localStorage elements within Header

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,8 @@ export default function Header(props) {
   return (
     <div className="header">
       <h1>ProtoCards</h1>
-      <p>Welcome {props.userName}!</p>
-      <p>You've completed {props.completion} out of 30 questions!</p>
+      <p>Welcome <span>{props.userName}</span>!</p>
+      <p>You've completed <span>{props.completion}</span> out of 30 questions!</p>
     </div>
     )
 }
