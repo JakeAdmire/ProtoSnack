ceafabb1655fa584b299c988d4ab47096e2a6139
Jake <jakeadmire1@gmail.com>
2019-02-25 13:34:54 -0700

    Fix arrow button orientation

--- src/Card.js
+++ src/Card.js
@@ -13,17 +13,21 @@ export default class Card extends Component {
     let cardClass = this.state.showAnswer ? 'back' : '';
     let cardElements = this.state.showAnswer 
       ? <div className="card-back"><h3>The Correct Solution is:</h3> <h4>{content}</h4></div>
-      : <div className="card-front"><h3>{content.proplem}</h3> <h4>{content.snippet}</h4></div>;
+      : <div className="card-front"><h3>{content.problem}</h3> <h4>{content.snippet}</h4></div>;
     return (
       <div className={`card ${cardClass}`}>
         <button onClick={() => {
               this.setState({showAnswer: false});
               this.props.prevCard();
-            }} className="left">PREVIOUS</button>
+            }} className="left">PREVIOUS
+          <span></span>
+        </button>
         <button onClick={() => {
               this.setState({showAnswer: false});
               this.props.nextCard();
-            }} className="right">SKIP</button>
+            }} className="right">SKIP
+          <span></span>
+        </button>
         <span onClick={() => this.setState({showAnswer: !this.state.showAnswer})} className='card-counter'>{this.props.cardNumber + 1}</span>
         {cardElements}
       </div>
