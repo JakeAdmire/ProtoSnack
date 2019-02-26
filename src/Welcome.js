e0e519d05d8f0e53721cd17cfaf633113bada175
Jake <jakeadmire1@gmail.com>
2019-02-25 22:20:02 -0700

    Style popup container text

--- src/Welcome.js
+++ src/Welcome.js
@@ -8,7 +8,7 @@ export default class Welcome extends Component {
   }
   render() {
     console.log(this.state.firstTime);
-    let welcomeHeader = this.state.firstTime ? "Welcome to ProtoSnack!" : "Welcome back to ProtoSnack!";
+    let welcomeHeader = this.state.firstTime ? <h1>Welcome to <span>ProtoSnack!</span></h1> : <h1>Welcome back to <span>ProtoSnack!</span></h1>;
     let welcomeTitle = this.state.firstTime ? "We see that this is your first time here!" : "We've missed you!";
     let welcomeDescript = this.state.firstTime ? "ProtoSnack is an app for practicing your basic JavaScript string and array prototypes!" : "ProtoSnack remembered your progress while you were away!";
     let welcomeGoodbye = this.state.firstTime ? "ProtoSnack will remember your progress for you, so feel free to relax and study!" : "Go ahead and study to your heart's content!";
@@ -16,8 +16,10 @@ export default class Welcome extends Component {
     return (
       <div className="screen hide">
         <div className="welcome">
-          <h1>{welcomeHeader}</h1>
-          <h3>{welcomeTitle}</h3>
+          <div className="title">
+            {welcomeHeader}
+            <h3>{welcomeTitle}</h3>
+          </div>
           <p>{welcomeDescript}</p>
           <p>{welcomeGoodbye}</p>
           {continueButton}
