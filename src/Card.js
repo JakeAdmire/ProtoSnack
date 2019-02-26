63bf457c87b0f63b77c646f66597b8a44c1fcf29
Jake <jakeadmire1@gmail.com>
2019-02-25 18:55:54 -0700

    Seperate card code snippet from rest of elements

--- src/Card.js
+++ src/Card.js
@@ -13,7 +13,7 @@ export default class Card extends Component {
     let cardClass = this.state.showAnswer ? 'back' : '';
     let cardElements = this.state.showAnswer 
       ? <div className="card-back"><h3>The Correct Solution is:</h3> <h4>{content}</h4></div>
-      : <div className="card-front"><h3>{content.problem}</h3> <h4>{content.snippet}</h4></div>;
+      : <div className="card-front"><h3>What prototype would you use to..</h3><h3>{content.problem}</h3> <h4 className="snippet">{content.snippet}</h4></div>;
     return (
       <div className={`card ${cardClass}`}>
         <button onClick={() => {
