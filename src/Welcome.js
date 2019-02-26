cf2f7d6f9d82b7718533a286aac36b47fa2f4c46
Jake <jakeadmire1@gmail.com>
2019-02-25 22:40:19 -0700

    Style input and button on popup

--- src/Welcome.js
+++ src/Welcome.js
@@ -12,7 +12,7 @@ export default class Welcome extends Component {
     let welcomeTitle = this.state.firstTime ? "We see that this is your first time here!" : "We've missed you!";
     let welcomeDescript = this.state.firstTime ? "ProtoSnack is an app for practicing your basic JavaScript string and array prototypes!" : "ProtoSnack remembered your progress while you were away!";
     let welcomeGoodbye = this.state.firstTime ? "ProtoSnack will remember your progress for you, so feel free to relax and study!" : "Go ahead and study to your heart's content!";
-    let continueButton = this.state.firstTime ? <div><input type="text" placeholder="enter name here.." ></input><button>Continue</button></div> : <div><button>Thanks!</button></div>;
+    let continueButton = this.state.firstTime ? <div className="inputContainer"><input type="text" placeholder="enter name here.." ></input><button>Continue</button></div> : <div><button>Thanks!</button></div>;
     return (
       <div className="screen hide">
         <div className="welcome">
