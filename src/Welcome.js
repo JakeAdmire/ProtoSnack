04c95b02acef6b0c3d853a045d9012bbcf4bc6cb
Jake <jakeadmire1@gmail.com>
2019-02-26 08:52:45 -0700

    Empty out Welcome Popup if user has been to site before

--- src/Welcome.js
+++ src/Welcome.js
@@ -6,6 +6,10 @@ export default class Welcome extends Component {
     super();
     this.state = { firstTime: true, userName: '' };
   }
+  componentDidMount = () => {
+    console.log('mounteeeeng');
+    this.setState({firstTime: localStorage.getItem('firstTime') ? false : true});
+  }
   setName = () => {
     const value = document.querySelector('.userSubmit').value;
     this.setState({userName: value});
@@ -13,14 +17,16 @@ export default class Welcome extends Component {
   storeName = (e) => {
     e.preventDefault();
     localStorage.setItem('userName', this.state.userName);
+    localStorage.setItem('firstTime', this.state.userName.length ? false : true);
+    this.setState({firstTime: this.state.userName.length ? false : true});
   }
   render() {
-    console.log(this.state.userName);
-    let welcomeHeader = this.state.firstTime ? <h1>Welcome to <span>ProtoSnack!</span></h1> : <h1>Welcome back to <span>ProtoSnack!</span></h1>;
-    let welcomeTitle = this.state.firstTime ? "We see that this is your first time here!" : "We've missed you!";
-    let welcomeDescript = this.state.firstTime ? "ProtoSnack is an app for practicing your basic JavaScript string and array prototypes!" : "ProtoSnack remembered your progress while you were away!";
-    let welcomeGoodbye = this.state.firstTime ? "ProtoSnack will remember your progress for you, so feel free to relax and study!" : "Go ahead and study to your heart's content!";
-    let continueButton = this.state.firstTime ? <form className="inputContainer"><input onChange={this.setName} className="userSubmit" type="text" placeholder="enter name here.." ></input><button onClick={this.storeName}>Submit</button></form> : <div><button>Thanks!</button></div>;
+    console.log('firsttime', this.state.firstTime);
+    let welcomeHeader = this.state.firstTime ? <h1>Welcome to <span>ProtoSnack!</span></h1> : '';
+    let welcomeTitle = this.state.firstTime ? "We see that this is your first time here!" : '';
+    let welcomeDescript = this.state.firstTime ? "ProtoSnack is an app for practicing your basic JavaScript string and array prototypes!" : '';
+    let welcomeGoodbye = this.state.firstTime ? "ProtoSnack will remember your progress for you, so feel free to relax and study!" : '';
+    let continueButton = this.state.firstTime ? <form className="inputContainer"><input onChange={this.setName} className="userSubmit" type="text" placeholder="enter name here.." ></input><button onClick={this.storeName}>Submit</button></form> : '';
     return (
       <div className="screen hide">
         <div className="welcome">
