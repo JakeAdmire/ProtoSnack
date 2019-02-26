fdb230c4ca6657c1c991419e468574982d61ac7b
Jake <jakeadmire1@gmail.com>
2019-02-26 07:51:07 -0700

    Add username to local storage on chick

--- src/Welcome.js
+++ src/Welcome.js
@@ -4,15 +4,23 @@ import React, { Component } from 'react';
 export default class Welcome extends Component {
   constructor() {
     super();
-    this.state = { firstTime: true };
+    this.state = { firstTime: true, userName: '' };
+  }
+  setName = () => {
+    const value = document.querySelector('.userSubmit').value;
+    this.setState({userName: value});
+  }
+  storeName = (e) => {
+    e.preventDefault();
+    localStorage.setItem('userName', this.state.userName);
   }
   render() {
-    console.log(this.state.firstTime);
+    console.log(this.state.userName);
     let welcomeHeader = this.state.firstTime ? <h1>Welcome to <span>ProtoSnack!</span></h1> : <h1>Welcome back to <span>ProtoSnack!</span></h1>;
     let welcomeTitle = this.state.firstTime ? "We see that this is your first time here!" : "We've missed you!";
     let welcomeDescript = this.state.firstTime ? "ProtoSnack is an app for practicing your basic JavaScript string and array prototypes!" : "ProtoSnack remembered your progress while you were away!";
     let welcomeGoodbye = this.state.firstTime ? "ProtoSnack will remember your progress for you, so feel free to relax and study!" : "Go ahead and study to your heart's content!";
-    let continueButton = this.state.firstTime ? <div className="inputContainer"><input type="text" placeholder="enter name here.." ></input><button>Continue</button></div> : <div><button>Thanks!</button></div>;
+    let continueButton = this.state.firstTime ? <form className="inputContainer"><input onChange={this.setName} className="userSubmit" type="text" placeholder="enter name here.." ></input><button onClick={this.storeName}>Submit</button></form> : <div><button>Thanks!</button></div>;
     return (
       <div className="screen hide">
         <div className="welcome">
