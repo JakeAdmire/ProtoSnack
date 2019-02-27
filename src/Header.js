ad9235afe5890bb3e8de31fcd47ab120a791ff68
Jake <jakeadmire1@gmail.com>
2019-02-27 06:33:46 -0700

    Complete functionality for keeping track of completion progress

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ export default function Header(props) {
     <div className="header">
       <h1>ProtoSnack!</h1>
       <p>Welcome <span>{props.userName}</span>!</p>
-      <p><span>{props.completion}</span> out of <span>30</span> questions complete!</p>
+      <p><span>{localStorage.getItem('currNum')}</span> out of <span>30</span> questions complete!</p>
     </div>
     )
 }
