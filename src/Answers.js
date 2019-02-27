69f433de43130685d5a5e711842d8f8ac4e3747c
Jake <jakeadmire1@gmail.com>
2019-02-26 17:55:31 -0700

    Add method to Answers.js to sort the answers array before it appends

--- src/Answers.js
+++ src/Answers.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Solution from './Solution.js';
 
 
-export default function Answers(props) {
-  let answersArray = [];
-  for (let i = 0; i < 3; i++) {
-    let randomIndex = Math.floor(Math.random() * props.answers.length);
-    answersArray.push(props.answers[randomIndex]);
-  };
-  return (
-    <div className="answers">
-      {answersArray.map((answer) => {
-        return <Solution  answer={answer.name}
-                          flipCard={props.flipCard} />
-        })
-      }
-      <Solution answer={props.correctAnswer}
-                flipCard={props.flipCard} />
-    </div>
-  )
+export default class Answers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { answersArray: []};
+  }
+  componentDidMount() {
+    let wrongAnswers = [];
+    for (let i = 0; i < 3; i++) {
+      let randomIndex = Math.floor(Math.random() * this.props.answers.length);
+      wrongAnswers.push(this.props.answers[randomIndex].name);
+    }
+    wrongAnswers.push(this.props.correctAnswer);
+    this.shuffleArray(wrongAnswers)
+  }
+  shuffleArray(array) {
+    this.setState({ answersArray: array.sort() })
+  }
+  render() {
+    return (
+      <div className="answers">
+        {this.state.answersArray.map((answer) => {
+          console.log(answer);
+          return <Solution  answer={answer}
+                            flipCard={this.props.flipCard} />
+          })
+        }
+      </div>
+      )
+  }
 }
