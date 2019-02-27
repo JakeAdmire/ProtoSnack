18d936685e06d5119dc0eca349ed7d44a34bf126
Jake <jakeadmire1@gmail.com>
2019-02-27 06:28:07 -0700

    Change 'ProtoCards' title to 'ProtoSnack'

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header(props) {
   return (
     <div className="header">
-      <h1>ProtoCards</h1>
+      <h1>ProtoSnack!</h1>
       <p>Welcome <span>{props.userName}</span>!</p>
       <p><span>{props.completion}</span> out of <span>30</span> questions complete!</p>
     </div>
